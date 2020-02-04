9d0de8fbddde3759d8e59d137b1cb050e4657a26
sophiaevanisko <sophiajevanisko@gmail.com>
2020-02-04 10:04:26 -0500

    updated so button logs number of clicks

--- app/src/main/java/com/evaniskosophia/lab0/MainActivity.java
+++ app/src/main/java/com/evaniskosophia/lab0/MainActivity.java
@@ -3,12 +3,23 @@ package com.evaniskosophia.lab0;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button myButton2;
+    int clicks = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myButton2=findViewById(R.id.myButton);
+        myButton2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                clicks += 1;
+                System.out.println(clicks);
+            }
+        });
+
     }
 }
